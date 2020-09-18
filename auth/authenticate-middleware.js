77154647f70d746d89941fee1ab96184f7dc4f6b
chaycesol <chayce.solchaga@gmail.com>
2020-09-18 08:47:15 -0700

    auth setup complete

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,24 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require('jsonwebtoken');
+
+const secret = require('./auth-helpers');
+
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, secret.jwtSecret, (error, decodedToken) => {
+      if (error) {
+        res.status(401).json({ message: `Invalid token` });
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ you: `shall not pass! JWT error` });
+  }
 };
